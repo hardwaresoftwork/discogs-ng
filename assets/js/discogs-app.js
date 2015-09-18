7362bc8a656531fa57b37f6fc07cb458f723d64c
Erik Woitschig <erik@sourcloud.com>
2015-09-18 20:06:06 +0200

    ga changed to track events

--- assets/js/discogs-app.js
+++ assets/js/discogs-app.js
@@ -28,3 +28,33 @@ otherwise({
   redirectTo: '/home'
 });
 }]);
+
+SourSoundApp.factory('myGoogleAnalytics', [
+  '$rootScope', '$window', '$location',
+  function ($rootScope, $window, $location) {
+
+    var myGoogleAnalytics = {};
+    console.log('yo');
+    /**
+     * Set the page to the current location path
+     * and then send a pageview to log path change.
+     */
+    myGoogleAnalytics.sendPageview = function() {
+      if ($window.ga) {
+        $window.ga('set', 'page', $location.path());
+        $window.ga('send', 'pageview');
+      }
+    }
+
+    // subscribe to events
+    $rootScope.$on('$viewContentLoaded', myGoogleAnalytics.sendPageview);
+
+    return myGoogleAnalytics;
+  }
+])
+SourSoundApp.run([
+  'myGoogleAnalytics',
+  function(myGoogleAnalytics) {
+      // inject self
+  }
+]);
--- assets/js/ga.js
+++ assets/js/ga.js
@@ -9,4 +9,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-59815538-1', 'auto');
-ga('send', 'pageview');
+//ga('send', 'pageview');
